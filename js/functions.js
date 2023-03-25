17248a4f2e67ee6db371c5f01d816f14953f9172
Parsa Yazdani <3parsa3@gmail.com>
2023-03-26 03:52:02 +1100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -73,17 +73,19 @@ function cookie(name){
     if(match)
         return match[2];
 }
-function setCookie(name, value, days){
-    var expires = "";
-    if(days){
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
+if(typeof setCookie === "undefined"){ 
+    function setCookie(name, value, days){
+        var expires = "";
+        if(days){
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
 
-    if(typeof domain === 'undefined')
-        domain = window.location.hostname;
-    document.cookie = name + "=" + (value || "")  + expires + ";path=/;domain=." + domain;
+        if(typeof domain === 'undefined')
+            domain = window.location.hostname;
+        document.cookie = name + "=" + (value || "")  + expires + ";path=/;domain=." + domain;
+    }
 }
 function delete_cookie(name){
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
